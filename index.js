0306af26a010a58c505ad9041fd2029735f168e8
Nithyadevi <nithi2218@gmail.com>
2022-03-18 14:25:40 +1100

    Assignment Completed

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const farmAnimals = 'cow horse sheep pig chicken';
 
+const [ moo, neigh, baa, oink, cluck ] = farmAnimals.split(" ");
+const [bessie, , dolly, babe, little] = farmAnimals.split(" ");
+const [blackAndWhite, , black, pink] = farmAnimals.split(" ")
+
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+const [red, orange, yellow, green, blue, indigo, violet] = colors
+const[r, o, y, g, b, indg, v] = colors
+
 
 const muppet = {
   muppetName: 'Miss Piggy',
@@ -10,9 +17,15 @@ const muppet = {
   partner: 'Kermit'
 };
 
+const muppetName = muppet.muppetName;
+const color = muppet.color;
+const song = muppet.song;
+const job = muppet.job;
+const partner = muppet.partner;
+
 const nestedMuppet = {
   nestedName: 'Kermit',
-  nestedColor: 'green',
+  nestedName: 'green',
   album: {
     theMuppetMovie: {
       song1: 'Rainbow Connection',
@@ -25,23 +38,10 @@ const nestedMuppet = {
   nestedPartner: 'Miss Piggy'
 };
 
-// Strings
-
-// 1. Use destructuring to assign appropriate variables based on the sounds animals make.
-
-// 2. Bolt the horse wandered off, so just give us four animals, and let's name them bessie, dolly, babe, and little.
-
-// 3. Little the chicken had to go back to the coop, so now we're left with three. Let's use color variables of blackAndWhite, black, and pink.
-
-// Arrays
-
-// 4. Use destructuring to assign appropriate variables using the color names.
-
-// 5. Some people have a really hard time picking out indigo, so let's leave that one out, using the first letter of each color as the variable names.
-
-// 6. But wait! Indigo is now feeling *super* left out. Let's only assign indigo using indg. 
-
-// Objects
+const {album: {theMuppetMovie: {song2}}} = nestedMuppet;
+const {album: {theMuppetMovie: {song4}}} = nestedMuppet;
+const {nestedPartner} = nestedMuppet;
+const {nestedJob} = nestedMuppet;
 
 // 7. Use destructuring to assign all variables using the keys as the variable names
 
